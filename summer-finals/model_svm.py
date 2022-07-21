7e0800b47000d8ce5cf3cdb3c6be876c4991ecbe
dvarkless <amir911157@yandex.ru>
2022-07-21 18:46:19 +0300

    Improve model + make it mathematicly correct

--- summer-finals/model_svm.py
+++ summer-finals/model_svm.py
@@ -1,24 +1,43 @@
 import numpy as np
-from image_feature_detector import (get_features, get_plain_data,
-                                    threshold_high, threshold_low,
-                                    threshold_mid)
+from image_feature_detector import PCA_transform, get_features, get_plain_data
 from metrics import return_accuracy
 from model_base import BaseModel
 
 
 class SVM_Model(BaseModel):
+    """ 
+        Модель Машины опорных векторов для мультиклассовой классификации.
+
+        Строит гиперплоскость с максимальным расстоянием от точек в пространстве,
+        разделяющую входные данные на 2 класса.
+
+        Для классификации на 3 и более класса надо построить несколько моделей
+        и сравнить их результаты способом 'один против одного' или 
+        'один против остальных' (One vs One, One vs Rest).
+        На данный момент реализован способ One vs Rest
+
+        Применяется функция стоимости Soft Margin Loss:
+        https://towardsdatascience.com/svm-implementation-from-scratch-python-2db2fc52e5c2#66a2
+
+        Для работы функции стоимости надо ответы на тренировочной выборке
+        подавать в виде: [-1, -1, 1, -1],  где позиция единицы - номер класса точки.
+
+    """
+
     def __init__(self, data_converter=None, custom_params=None) -> None:
         super().__init__(data_converter, custom_params)
 
     def fit(self, data):
         y, x = self._splice_data(data)
-        y = self.get_probabilities(y)
+        y = self._ovr_split(y)
         self.params = np.zeros_like(x.T @ y)
         num_samples = x.shape[0]
-        
+
         # забираем гиперпараметры, если есть
-        learning_rate = getattr(self, 'learning_rate', 0.1)  
+        learning_rate = getattr(self, 'learning_rate', 0.1)
         epochs = getattr(self, "epochs", 100)
+        k = getattr(self, "regularization", 1)
+        debug = getattr(self, "debug", False)
 
         for epoch in range(epochs):
             indexes = np.random.permutation(num_samples)
@@ -27,29 +46,88 @@ class SVM_Model(BaseModel):
             for x_batch, y_batch in self._iter_batches(x, y):
                 y_pred = self._forward(x_batch)
                 self.params -= self._compute_gradient(
-                    x_batch, y_pred, y_batch, learning_rate, k=1)
+                    x_batch, y_pred, y_batch, learning_rate, k=k)
 
-            loss = self.hinge_cost(x, y)
+            loss = self.soft_margin_loss(self._forward(x), y)
 
-        return self
+            if debug:
+                print(f'-----epoch {epoch+1}/{epochs}-----  ', end='')
+                print(f'Current cost: {loss.sum():.2f}')
 
-    def hinge_cost(self, x, y, k=1):
-        loss = 1 - y * self._forward(x)
-        loss[loss < 0] = 0
+        if debug:
+            print(
+                f'''params properties: mean={self.params.mean()},  
+                std={self.params.std()}, max={self.params.max()}, 
+                min={self.params.min()}
+                ''')
 
-        loss = k * loss.mean(axis=0)
+            return self
 
-        cost = 0.5 * np.sum(self.params.T @ self.params, axis=0) + loss
-        return cost
+    def hinge_loss(self, y_pred, y):
+        """
+            Применяется формула:
+            max(0, 1 - y_i*(w*x_i+b)) - для каждого значения в массиве
+
+            inputs:
+                y_pred - model's output
+                y - training data answer
+            
+            output:
+                np.array (shape = n_classes)
+        """
+        return np.vectorize(lambda x: x if x > 0 else 0)(1-y*y_pred).mean(axis=0)
+
+    def soft_margin_loss(self, y_pred, y, k=1):
+        """
+            Формула:
+            J = k * ||w||^2 + 1/N * Sum(max(0, 1 - y_i*(w*x_i+b)))
+
+            inputs:
+                y_pred - model's output
+                y - training data answer
+                k - regularization strength
+            
+            output:
+                np.array (shape = n_classes)
+        """
+        return (self.hinge_loss(y_pred, y) + (k * self.params.T @ self.params)).mean(axis=0)
+
+    def _ovr_choose(self, x):
+        """
+            Возвращаем позицию наибольшего значения
+
+            input:
+                x - input array, 2 dims
+
+            output - np.ndarray, 1 dim
+        """
+        return np.argmax(x, axis=1) + 1
+
+    def _ovr_split(self, x):
+        """
+            Получаем единицу на позиции значения ответа, как в 
+            .get_probabilities(), только остальные значения 
+            приравниваются к -1.
+
+            input:
+                x - input array, 2 dims
+
+            output - np.ndarray, 1 dim
+        """
+        return self.get_probabilities(x, custom_fun=self._plus_minus_one)
+
+    def _plus_minus_one(self, x):
+        """
+            Конвертирует каждое значение в 1 или -1
+        """
+        num = getattr(self, "num_classes")
+        out = np.zeros(num) - 1
+        out[int(x) - 1] = 1
+        return out
 
     def _compute_gradient(self, x, y_pred, y, lr=0.01, k=1) -> np.ndarray:
-        loss = 1 - y * y_pred
-        loss[loss <= 0] = 0
-        loss[loss > 0] = 1
-        positive_part = loss.mean(axis=0)
-
         diff = np.zeros_like(self.params)
-        diff = self.params - (k * positive_part * (x.T @ y))
+        diff = self.params - (k * self.soft_margin_loss(y_pred, y) * (x.T @ y))
 
         return diff * lr
 
@@ -58,10 +136,10 @@ class SVM_Model(BaseModel):
 
     def predict(self, x):
         self.data = x
-        return self.get_labels(self._softmax(self.data @ self.params))
+        return self._ovr_choose(self.data @ self.params)
 
 
-if __name__=='__main__':
+if __name__ == '__main__':
     my_data = np.genfromtxt("datasets/light-train.csv",
                             delimiter=",", filling_values=0)
     test_data = np.genfromtxt(
@@ -69,11 +147,20 @@ if __name__=='__main__':
 
     hp = {
         'num_classes': 26,
+        'epochs': 200,
+        'batch_size': 10,
+        'learning_rate': 0.0001,
+        'regularization': 0.01,
         'normalization': True,
         'shift_column': True,
+        'debug': True,
+
     }
     num_classes = 26
-    model = SVM_Model(data_converter=get_plain_data, custom_params=hp).fit(my_data)
+
+    pca_transformer = PCA_transform(100).fit(my_data)
+    model = SVM_Model(data_converter=pca_transformer,
+                      custom_params=hp).fit(my_data)
 
     ans_test = model.predict(test_data[:, 1:])
     ans_train = model.predict(my_data[:, 1:])
