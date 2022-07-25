05df44e0dd05dbb5e4dae920d049a054540604de
dvarkless <amir911157@yandex.ru>
2022-07-25 22:11:30 +0300

    rework accuracy function; add f1 metric; all metrics now average by macro

--- summer-finals/metrics.py
+++ summer-finals/metrics.py
@@ -1,22 +1,34 @@
-import numpy as np
-from sklearn.metrics import ConfusionMatrixDisplay
 import matplotlib.pyplot as plt
+from sklearn.metrics import (ConfusionMatrixDisplay, accuracy_score,
+                             precision_score, recall_score, f1_score)
 
 
-def show_metrics_matrix(preds, ans):
+def show_metrics_matrix(preds, answer):
     labels = list("ABCDEFGHIJKLMNOPQRS")
     ConfusionMatrixDisplay.from_predictions(
+        answer,
         preds,
-        ans,
         labels,
         normalize="true",
     )
     plt.show()
 
 
-def return_accuracy(preds, ans):
-    return np.count_nonzero(preds == ans) / len(ans)
+def return_precision(preds, answer):
+    return precision_score(answer, preds, average='macro', zero_division=1)
 
 
-def print_accuracy(preds, ans):
-    print(np.count_nonzero(preds == ans) / len(ans))
+def return_recall(preds, answer):
+    return recall_score(answer, preds, average='macro', zero_division=1)
+
+
+def return_f1(preds, answer):
+    return f1_score(answer, preds, average='macro', zero_division=1)
+
+
+def return_accuracy(preds, answer):
+    return accuracy_score(answer, preds)
+
+
+def print_accuracy(preds, answer):
+    print(accuracy_score(answer, preds))
