2b7d612d0a0f929900a8666c95a92352ce7d274e
dvarkless <amir911157@yandex.ru>
2022-07-28 15:06:42 +0300

    Add auc_roc metric

--- summer-finals/metrics.py
+++ summer-finals/metrics.py
@@ -1,10 +1,10 @@
 import matplotlib.pyplot as plt
-from sklearn.metrics import (ConfusionMatrixDisplay, accuracy_score,
-                             precision_score, recall_score, f1_score)
+from sklearn.metrics import (ConfusionMatrixDisplay, accuracy_score, f1_score,
+                             precision_score, recall_score, roc_auc_score)
 
 
 def show_metrics_matrix(preds, answer):
-    labels = list("ABCDEFGHIJKLMNOPQRS")
+    labels = list("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
     ConfusionMatrixDisplay.from_predictions(
         answer,
         preds,
@@ -26,14 +26,25 @@ def return_f1(preds, answer):
     return f1_score(answer, preds, average='macro', zero_division=1)
 
 
+def return_roc_auc_ovo(preds, answer):
+    return roc_auc_score(answer, preds, average='macro', multi_class='ovo')
+
+
+def return_roc_auc_ovr(preds, answer):
+    return roc_auc_score(answer, preds, average='macro', multi_class='ovr')
+
+
 def return_accuracy(preds, answer):
     return accuracy_score(answer, preds)
+
 
 def predictions_mean(preds, answer):
     return preds.mean()
 
+
 def predictions_std(preds, answer):
     return preds.std()
+
 
 def print_accuracy(preds, answer):
     print(accuracy_score(answer, preds))
