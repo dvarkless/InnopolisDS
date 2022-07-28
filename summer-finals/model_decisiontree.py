e39d98d1490921137a830ae6b5a2c16aa199a4fd
dvarkless <amir911157@yandex.ru>
2022-07-28 23:04:24 +0300

    Fix major bug with data conversion + balance weights feature + fixes

--- summer-finals/model_decisiontree.py
+++ summer-finals/model_decisiontree.py
@@ -1,4 +1,5 @@
 from itertools import combinations
+from typing import Any
 
 import numpy as np
 from image_feature_detector import *
@@ -27,7 +28,7 @@ class Question:
 
     def __repr__(self):
         return f'{self.level}) Is var #{self.feature} > {self.question_val:.3f}?\n \
-                (G={self.gini:.2f})((L({self.left_len})->{self.left_mean})R({self.right_len})->{self.right_mean})'
+                (G={self.gini:.2f})((L({self.left_len})->{self.left_mean:.3f})R({self.right_len})->{self.right_mean:.3f})'
 
     def split_by_question(self, data: np.ndarray):
         is_true = data[:, self.feature] > self.question_val
@@ -40,9 +41,9 @@ class Question:
 
         return data[is_false], data[is_true]
 
-    def my_gini(self, data: np.ndarray, min_samples: int = 0):
+    def my_gini(self, data: np.ndarray, min_samples: int = 0, balance_coeff: float = 1):
         self.gini = DecisionTreeClassifier.get_gini(
-            *self.split_by_question(data), min_samples)
+            *self.split_by_question(data), min_samples, balance_coeff)
         return self.gini
 
     def hold_branches(self, left_branch, right_branch):
@@ -55,13 +56,13 @@ class Question:
 class Leaf:
     def __init__(self, level: int, data: np.ndarray) -> None:
         self.level = level
-        self.prediction = round(data[:, -1].mean())
+        self.prediction = data[:, -1].mean()
 
-    def __call__(self, row: np.ndarray):
+    def __call__(self, row: Any):
         return self.prediction
 
     def __repr__(self):
-        return f'{self.level}) return {self.prediction}'
+        return f'{self.level}) return {self.prediction:.3f}'
 
 
 class DecisionTreeClassifier(BaseModel):
@@ -78,22 +79,23 @@ class DecisionTreeClassifier(BaseModel):
         super().__init__(custom_params)
         self.tree_root = []
 
+        if getattr(self, 'balance_coeff', 1) <= 0:
+            raise ValueError(
+                f'Balance coefficient could not be negative or zero, got {getattr(self,"balance_coeff")}')
+
     def fit(self, dataset: np.ndarray):
         self.y, self.x = self._splice_data(dataset)
         if getattr(self, 'tree_type') in ('binary', 'multilabel'):
             self.y = self.y[:, np.newaxis]
         elif getattr(self, 'tree_type') == 'multilabel_ovo':
-            self.y = self._ovo_split(self.get_probabilities(self.y))
+            self.y = self._ovo_split(self.y)
         elif getattr(self, 'tree_type') == 'multilabel_ovr':
             self.y = self.get_probabilities(self.y)
+            if not hasattr(self, 'balance_coeff'):
+                self.balance_coeff = getattr(self, 'num_classes')//2
         else:
             raise AssertionError(
-                f"Unknown tree type: {getattr(self, 'tree_type')}")
-
-        if getattr(self, 'tree_type') in ('multilabel_ovo', 'multilabel_ovr'):
-            num_classes = getattr(self, 'num_classes')
-        else:
-            num_classes = 1
+                f"Unknown tree type: '{getattr(self, 'tree_type')}'")
 
         self.tree_root = []
         self.question_list = self._return_ranges(
@@ -101,9 +103,15 @@ class DecisionTreeClassifier(BaseModel):
 
         for i in range(self.y.shape[1]):
             data = np.hstack((self.x, self.y[:, i][:, np.newaxis]))
+            data = self._pick_data(data)
+
             self.tree_root.append(self.build_tree(0, data))
             if hasattr(self, '_tick'):
                 self._tick()
+
+    def _pick_data(self, data):
+        is_legit = data[:, -1] >= 0
+        return data[is_legit]
 
     def _return_ranges(self, min_val: float, max_val: float, mean_val: float, num: int):
         num = (num+1)//2
@@ -112,7 +120,7 @@ class DecisionTreeClassifier(BaseModel):
             left = -(np.geomspace(mean_val, -min_val +
                      mean_val*2, num=num) - mean_val*2)[::-1]
         else:
-            left = np.geomspace(mean_val, min_val, num=num)[::-1]
+            left = np.geomspace(mean_val, min_val+mean_val/100, num=num)[::-1]
 
         return np.hstack((left, right[1:]))
 
@@ -124,7 +132,7 @@ class DecisionTreeClassifier(BaseModel):
 
         my_question = self.create_node(data, level, window_size)
 
-        if my_question.my_gini(data, min_samples=getattr(self, 'min_samples')) == 0:
+        if my_question.my_gini(data, getattr(self, 'min_samples'), getattr(self, 'balance_coeff', 1)) == 0:
             return Leaf(level, data)
         if level == getattr(self, 'max_depth', 10):
             return Leaf(level, data)
@@ -137,9 +145,10 @@ class DecisionTreeClassifier(BaseModel):
         return my_question.hold_branches(left_branch, right_branch)
 
     def predict(self, input_data: np.ndarray):
+        self.data = input_data
         out = np.zeros((input_data.shape[0], self.y.shape[1]))
         for i, tree in enumerate(self.tree_root):
-            out[:, i] = np.array(list(map(tree, input_data)))
+            out[:, i] = np.array(list(map(tree, self.data)))
 
         if getattr(self, 'tree_type') in ('binary', 'multilabel'):
             return out
@@ -161,11 +170,14 @@ class DecisionTreeClassifier(BaseModel):
 
     def _ovo_split(self, x: np.ndarray):
         num_count = getattr(self, 'num_classes')
-        pairs = list(combinations(range(num_count), 2))
+        pairs = list(combinations(range(1, num_count+1), 2))
         out = np.zeros((x.shape[0], len(pairs)))
         for i, vals in enumerate(pairs):
-            val1, _ = vals
-            out[:, i] = x[:, val1]
+            val1, val2 = vals
+            out[:, i] = -x
+            out[:, i][out[:, i] == -val1] = 1
+            out[:, i][out[:, i] == -val2] = 0
+            out[:, i][out[:, i] < 0] = -1
 
         return out
 
@@ -177,7 +189,7 @@ class DecisionTreeClassifier(BaseModel):
             out[:, val1] += x[:, i]
             out[:, val2] -= x[:, i]
 
-        return self._ovr_choose(x)
+        return self._ovr_choose(out)
 
     def _array_window(self, arr, x, y, size):
         ''' Given a 2D-array, returns an nxn array whose "center" element is arr[x,y]'''
@@ -234,14 +246,30 @@ class DecisionTreeClassifier(BaseModel):
         my_question = Question(0, feature, question_val)
 
         left, right = my_question.split_by_question(data)
-        return self.get_gini(left, right, getattr(self, 'min_samples', 0))
+        return self.get_gini(left, right, getattr(self, 'min_samples', 0),
+                             getattr(self, 'balance_coeff', 1))
 
-    @ staticmethod
-    def get_gini(left: np.ndarray, right: np.ndarray, min_samples: float = 0):
+    @staticmethod
+    def get_gini(left: np.ndarray, right: np.ndarray, min_samples: float = 0, balance_coeff: float = 1):
+        if balance_coeff >= 1:
+            bal_true = balance_coeff
+            bal_false = 1
+        else:
+            bal_true = 1
+            bal_false = 1/balance_coeff
+
         l_counts = np.unique(left[:, -1], return_counts=True)[1]
         r_counts = np.unique(right[:, -1], return_counts=True)[1]
         len_L = l_counts.sum()
         len_R = r_counts.sum()
+        if len(l_counts):
+            l_counts[0] *= bal_false
+            try: l_counts[1] *= bal_true; 
+            except (IndexError): pass;
+        if len(r_counts):
+            r_counts[0] *= bal_false
+            try: l_counts[1] *= bal_true; 
+            except (IndexError): pass;
 
         if len_L <= min_samples or len_R <= min_samples:
             return 0
@@ -288,25 +316,26 @@ if __name__ == "__main__":
     my_metrics = [return_accuracy, predictions_mean, predictions_std]
     PCA_severe = PCA_transform(49).fit(
         training_data, answer_column=True)  # x16
-    PCA_severe = PCA_transform(49).fit(
+    PCA_lossy = PCA_transform(12).fit(
         training_data, answer_column=True)  # x16
 
     hp = {
         'data_converter': PCA_severe,
-        'sample_len': 32,
+        'sample_len': 16,
         'num_classes': 26,
         'window_size': -1,
         'min_samples': 4,
-        'max_depth': 7,
+        'max_depth': 5,
         'tree_type': 'multilabel_ovo',
 
     }
 
     TreeRunner = ModelRunner(DecisionTreeClassifier,
-                             defaults=hp, metrics=my_metrics)
+                             defaults=hp, metrics=my_metrics, responsive_bar=True)
     params_to_change = {
-        'data_converter': [PCA_severe]
+        'data_converter': [PCA_lossy]
     }
     TreeRunner.run(training_data, evaluation_input,
                    evaluation_answers, params_to_change, one_vs_one=False)
     # TreeRunner.model.print_tree()
+    input()
